feb75b9dac967487bf0733ee487a7c92b97b01ec
KatagiriMikiya <mikiya.katagiri@gmail.com>
2024-08-29 01:37:37 +0900

    [add]: 追加・更新

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .views import TaskCreate, TaskDetail, TaskList, TaskUpdate
 
 urlpatterns = [ 
-    path("", views.taskList)
+    path("", TaskList.as_view(), name='tasks'),
+    path("task/<int:pk>/", TaskDetail.as_view(), name = "task"),
+    path("create-task/", TaskCreate.as_view(), name = "create-task"),
+    path("edit-task/<int:pk>/", TaskUpdate.as_view(), name = "edit-task"),
 ]
