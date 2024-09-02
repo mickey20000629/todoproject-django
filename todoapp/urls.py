b38ade8a30e8282f83ad59fa1342bbab33fff441
KatagiriMikiya <mikiya.katagiri@gmail.com>
2024-09-02 23:26:22 +0900

    [Add]:

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import TaskCreate, TaskDetail, TaskList, TaskUpdate
+from .views import TaskCreate, TaskDelete, TaskDetail, TaskList, TaskListLoginView, TaskUpdate
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [ 
     path("", TaskList.as_view(), name='tasks'),
     path("task/<int:pk>/", TaskDetail.as_view(), name = "task"),
     path("create-task/", TaskCreate.as_view(), name = "create-task"),
     path("edit-task/<int:pk>/", TaskUpdate.as_view(), name = "edit-task"),
+    path("delete-task/<int:pk>/", TaskDelete.as_view(), name = "delete-task"),
+    path("login/", TaskListLoginView.as_view(), name = "login"),
+    path("logout/", LogoutView.as_view(next_page='login'), name = "logout")
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -2,27 +2,50 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 from todoapp.models import Task 
 
 
 # Create your views here.
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = "tasks"
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = "task"
+    login_url = reverse_lazy('login')
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy('tasks')
+    login_url = reverse_lazy('login')
 
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy('tasks')
+    login_url = reverse_lazy('login')
+
+class TaskDelete(LoginRequiredMixin, DeleteView):
+    model = Task
+    success_url = reverse_lazy('tasks')
+    context_object_name = "task"
+    login_url = reverse_lazy('login')
+
+
+class TaskListLoginView(LoginView):
+    fields = '__all__'
+    template_name = 'todoapp/login.html'
+    login_url = reverse_lazy('login')
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
