f8825eee17b063c9e0b78fbf20fb451b90746e15
KatagiriMikiya <mikiya.katagiri@gmail.com>
2024-09-03 16:54:20 +0900

    add

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskCreate, TaskDelete, TaskDetail, TaskList, TaskListLoginView, TaskUpdate
+from .views import TaskCreate, TaskDelete, TaskDetail, TaskList, TaskListLoginView, TaskUpdate, RegisterTodoApp
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [ 
@@ -9,5 +9,6 @@ urlpatterns = [
     path("edit-task/<int:pk>/", TaskUpdate.as_view(), name = "edit-task"),
     path("delete-task/<int:pk>/", TaskDelete.as_view(), name = "delete-task"),
     path("login/", TaskListLoginView.as_view(), name = "login"),
-    path("logout/", LogoutView.as_view(next_page='login'), name = "logout")
+    path("logout/", LogoutView.as_view(next_page='login'), name = "logout"),
+    path("register/", RegisterTodoApp.as_view(), name = "register")
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,16 +1,19 @@
+from django.forms import BaseModelForm
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 
-from todoapp.models import Task 
+from todoapp.models import Task
 
 
 # Create your views here.
@@ -18,34 +21,66 @@ class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = "tasks"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['programing'] = 'python'
+        # print(context)
+        context["tasks"] = context["tasks"].filter(user=self.request.user)
+
+        searchInputText = self.request.GET.get("search") or ""
+        if searchInputText:
+            context["tasks"] = context["tasks"].filter(title__startswith=searchInputText)
+        context["search"] = searchInputText
+        return context
+
+
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = "task"
-    login_url = reverse_lazy('login')
+    login_url = reverse_lazy("login")
+
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = "__all__"
-    success_url = reverse_lazy('tasks')
-    login_url = reverse_lazy('login')
+    fields = ["title", "description", "completed"]
+    success_url = reverse_lazy("tasks")
+    login_url = reverse_lazy("login")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = "__all__"
-    success_url = reverse_lazy('tasks')
-    login_url = reverse_lazy('login')
+    success_url = reverse_lazy("tasks")
+    login_url = reverse_lazy("login")
+
 
 class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
-    success_url = reverse_lazy('tasks')
+    success_url = reverse_lazy("tasks")
     context_object_name = "task"
-    login_url = reverse_lazy('login')
+    login_url = reverse_lazy("login")
 
 
 class TaskListLoginView(LoginView):
-    fields = '__all__'
-    template_name = 'todoapp/login.html'
-    login_url = reverse_lazy('login')
+    fields = "__all__"
+    template_name = "todoapp/login.html"
+    login_url = reverse_lazy("login")
 
     def get_success_url(self):
-        return reverse_lazy('tasks')
+        return reverse_lazy("tasks")
+
+
+class RegisterTodoApp(FormView):
+    template_name = "todoapp/register.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy("tasks")
+     
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return  super().form_valid(form)
